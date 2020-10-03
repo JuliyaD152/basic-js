d25098ae598d100cb1f274530f3e09b3f747ab55
Yuliya Belyavskaya <JuliyaD152@yandex.by>
2020-10-03 21:47:14 +0200

    docs: two solving tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,14 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(matrix) {
-  let counter = 0; 
-  for(let i = 0; i<matrix.length; i++){
-      console.log(matrix[i]);
-      for (let elem of matrix[i]){
-          if(elem === '^^'){
-              counter++;
-          }
-      }
-  }
-  return counter;
-};
+module.exports = function countCats(backyard) {
+    sum = 0;
+    backyard.forEach((element) => {
+      sum += element.filter(function(char) {return char === '^^'}).length;
+      });
+    return (sum);
+  };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if(Array.isArray(members) === false) return false;
+      let arr = [];
+  
+  members.forEach(element => {
+      if(typeof element != 'string') return;
+      element = element.trim();
+      if(element != ''){
+          arr.push(element[0].toUpperCase());
+      } 
+      if(arr.length === 0)return false;    
+  });
+  return arr.sort().join('');
+  };
+
